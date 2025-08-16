aff26c58073b922e4055ad0ab26a8b8bed9c1d9d
S-Hacker-I <zhackerneh@gmail.com>
2025-08-16 10:51:05 -0400

    sitemap added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-})
+  plugins: [
+    tailwindcss(),
+    react(),
+    sitemap({
+      hostname: 'https://waafi.pro/', // TODO: Replace with your domain
+    }),
+  ],
+});
